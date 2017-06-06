78ba9549174ffb97952f19ec5515c071fb914442
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-06 23:01:37 +0530

    Update large.java

--- large.java
+++ large.java
@@ -5,3 +5,16 @@ public static void main(string[] args)
 {
 int a,b,c,largest;
 Scanner s=new.Scanner(System.in);
+  a=s.nextint();
+  b=s.nextint();
+  c=s.nextint();
+  if(a>=b&&a>=c)
+    System.out.println(+a);
+  else if(b>=a&&b>=c)
+    System.out.println(+b);
+  else
+    System.out.println(+c);
+}
+}
+    
+  
