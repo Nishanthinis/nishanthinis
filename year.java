3aa5fff0181363235b6137bddc9d46344f34d303
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-06 23:31:17 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+public static void main(String args[]){
+int year,n;
+Scanner s=new Scanner(System.in);{
+year=s.nextInt();
+if(year%4=0)
+System.out.println(+year);
+}
+}
