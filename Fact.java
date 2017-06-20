1de7652a4eb442d3f5bc4cf66845f92b9ce8c173
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-20 17:25:25 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,14 @@
+package string;
+import java.util.Scanner;
+public class Fact {
+	public static void main(String[] args){
+		int i,n=5,fact=1;
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			}
+		System.out.println(+fact);
+	
+	}
+
+}
