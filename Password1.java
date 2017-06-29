8c18d68738df9d1411635a55c6d09adb9398d8a8
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-29 12:37:33 +0530

    Add files via upload

---
+++ Password1.java
@@ -0,0 +1,23 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Password1 {
+
+	public static void main(String[] args) {
+		String s1;
+		int k=0,m=0;
+		Scanner s=new Scanner(System.in);
+		s1=s.nextLine();
+		char[] ch=s1.toCharArray();
+		for(int i=0;i<s1.length();i++)
+		{
+		k=k+(int)ch[i];
+		
+		m=k/s1.length();
+		}
+		System.out.println((char)m);
+		
+	}
+
+}
