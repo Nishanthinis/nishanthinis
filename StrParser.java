60acc2c0f49a6789a6b93b63c2fd6e46b0a3b059
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-29 09:56:08 +0530

    Add files via upload

---
+++ StrParser.java
@@ -0,0 +1,13 @@
+package string;
+import java.util.Scanner;
+public class StrParser {
+	public static void main(String[] arg)
+	{
+	Scanner str=new Scanner(System.in);
+	String in="12345";
+	int a=Integer.parseInt(in);
+	System.out.println(a);
+	}
+
+
+}
