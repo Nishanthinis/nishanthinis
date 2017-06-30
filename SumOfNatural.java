f51a81019bc799d7e6b492fe60af4a279d5ccc0b
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-30 14:14:02 +0530

    Add files via upload

---
+++ SumOfNatural.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.Scanner;
+public class SumOfNatural {
+
+	public static void main(String[] args) {
+		int n;
+		int sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=1;i<n;i++){
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+
+}
