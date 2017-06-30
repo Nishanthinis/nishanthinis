eddbcafb0ec28368856ed5276ab6a085a3ac099c
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-06-30 14:07:31 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class Hello {
+
+	public static void main(String[] args) {
+	String s1;
+	int n;
+	Scanner s=new Scanner(System.in);
+		s1=s.nextLine();
+		n=s.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.print(" " +s1);
+			
+	}
+
+	}
+
+}
