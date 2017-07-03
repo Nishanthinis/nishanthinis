1d60c407567db19c51321a017e3191c26eee12c5
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-07-03 17:31:27 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,22 @@
+package guvi;
+import java.util.Scanner;
+public class Even {
+
+	public static void main(String[] args) {
+	int n1;
+	int n2;
+	Scanner s=new Scanner(System.in);
+	System.out.println("entre the no of n1:");
+	n1=s.nextInt();
+	System.out.println("enter the no of n2:");
+	n2=s.nextInt();
+for(int i=n1;i<n2;i++)
+{
+	if(i%2==0)
+	{
+		System.out.println(+i);
+	}
+}s.close();
+	}
+
+}
