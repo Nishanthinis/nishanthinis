838ba567249d888b53f4dbad50122fe01fe15c69
Nishanthinis <nishanthini.s1414058@ksrce.ac.in>
2017-07-25 12:07:01 +0530

    Add files via upload

---
+++ Workday.java
@@ -0,0 +1,20 @@
+package guvi;
+import java.util.Scanner;
+public class Workday {
+
+	public static void main(String[] args) {
+		String s1;
+		Scanner s=new Scanner(System.in);
+		s1=s.nextLine();
+		    if(s1.equals("sat")||(s1.equals("sun"))){
+		    	
+				System.out.println("false");
+			}
+			else{
+				System.out.println("true");
+			}
+		   
+		}
+}
+
+
